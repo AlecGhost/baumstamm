b2846551269d44e542d8ded3c845377e403feeb6
Alec Ghost <alec.ghost@protonmail.com>
2022-08-18 22:01:39 +0200

    refactor grid

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import PersonCard from './components/PersonCard.vue'
 import { ref } from 'vue'
-import Person from './types/Person'
-import ConnectorType from './types/ConnectorType'
-import Connector from './components/Connector.vue'
+import BaumstammGrid from './components/BaumstammGrid.vue'
 
-const person = ref<Person>({ firstName: "John", lastName: "Dow", dateOfBirth: "01.02.1234", dateOfDeath: null })
-const connectorType = ref<ConnectorType>(ConnectorType.Straight)
 const rows = ref(4)
 const columns = ref(5)
 
@@ -27,24 +20,6 @@ function zoomOut() {
 <template>
   <button @click="zoomOut">+</button>
   <button @click="zoomIn">-</button>
-  <section class="grid-container">
-    <div class="grid-item" v-for="n in rows * columns">
-      <Connector :connectorType="connectorType"></Connector>
-    </div>
-  </section>
-  <!-- <PersonCard :person="person"/> -->
+  <BaumstammGrid :columns="columns" :rows="rows"></BaumstammGrid>
 </template>
 
-<style scoped>
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(v-bind(columns), 1fr);
-  height: 90vh;
-}
-
-.grid-item {
-  background-color: green;
-  border-style: solid;
-  border-color: blue;
-}
-</style>
--- src/components/Connector.vue
+++ src/components/Connector.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, ref } from 'vue';
+import { ref } from 'vue';
 import ConnectorType from '../types/ConnectorType'
 
 const props = defineProps<{
