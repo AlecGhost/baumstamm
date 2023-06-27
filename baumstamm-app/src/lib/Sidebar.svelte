50c3608c01c72924b3398fca185fb5443ddd8bb7
Alec Ghost <alec.ghost@protonmail.com>
2023-06-27 12:13:07 +0200

    feature: add app shell and sidebar

---
+++ baumstamm-app/src/lib/Sidebar.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="card h-full">
+	<header class="card-header">
+		<div class="flex justify-center p-4">
+			<Avatar initials="XY" />
+		</div>
+		<div class="flex justify-center">Name</div>
+	</header>
+	<section class="p-4">Hello from the sidebar.</section>
+</div>
--- baumstamm-app/src/routes/+page.svelte
+++ baumstamm-app/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Sidebar from '$lib/Sidebar.svelte';
+	let showSidebar = false;
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead"><h1>Baumstamm</h1></svelte:fragment>
+			<svelte:fragment slot="trail">
+				<button
+					type="button"
+					class="btn-icon variant-filled"
+					on:click={() => (showSidebar = !showSidebar)}
+				/>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<svelte:fragment slot="sidebarRight">
+		<div class:hidden={showSidebar} class="h-full">
+			<Sidebar />
+		</div>
+	</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+	<!-- (footer) -->
+</AppShell>
