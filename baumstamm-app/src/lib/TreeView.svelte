ca907e59fc93a38e0e949e3a615656752ffe5e0a
Alec Ghost <alec.ghost@protonmail.com>
2023-07-07 17:29:58 +0200

    feature: add panzoom
    Replace manual SVG viewbox zooming and moving in `TreeView`
    with `panzoom` library.

--- baumstamm-app/src/lib/TreeView.svelte
+++ baumstamm-app/src/lib/TreeView.svelte
@@ -1,58 +1,13 @@
 <script lang="ts">
-	// movement
-	let x_pos = 0;
-	let y_pos = 0;
-	let moving = false;
+	import panzoom from 'panzoom';
 
-	function move(e: MouseEvent) {
-		if (moving) {
-			x_pos -= e.movementX;
-			y_pos -= e.movementY;
-		}
-	}
-
-	function setMoving() {
-		moving = true;
-	}
-
-	function unsetMoving() {
-		moving = false;
-	}
-
-	// zoom
-	let zoom = 1000;
-	let mouseover = false;
-
-	function wheel(e: WheelEvent) {
-		if (mouseover) {
-			let rel_x = x_pos / zoom;
-			let rel_y = y_pos / zoom;
-			zoom += e.deltaY;
-			x_pos = zoom * rel_x;
-			y_pos = zoom * rel_y;
-		}
-	}
-
-	function setMouseover() {
-		mouseover = true;
-	}
-
-	function unsetMouseover() {
-		mouseover = false;
+	function initPanzoom(node: HTMLElement) {
+		panzoom(node);
 	}
 </script>
 
-<svelte:window on:mouseup={unsetMoving} on:mousemove={move} on:wheel={wheel} />
-
-<div
-	class="h-full w-full tree-view"
-	on:mousedown={setMoving}
-	on:mouseover={setMouseover}
-	on:mouseout={unsetMouseover}
->
-	<svg viewBox="{x_pos} {y_pos} {zoom} {zoom}">
-        <!-- TODO: populate with nodes -->
-	</svg>
+<div class="h-full w-full tree-view" use:initPanzoom>
+	<!-- TODO: populate with nodes -->
 </div>
 
 <style>
