963c3da96a6d02774d7d41d9a06761b572ee436a
Alec Ghost <alec.ghost@protonmail.com>
2023-07-07 15:21:22 +0200

    refactor: move page contents to App component

--- baumstamm-app/src/routes/+page.svelte
+++ baumstamm-app/src/routes/+page.svelte
@@ -1,43 +1,5 @@
-<script lang="ts">
-	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
-	import Sidebar from '$lib/Sidebar.svelte';
-	import TreeView from '$lib/TreeView.svelte';
-
-	// sidebar
-	let showSidebar = false;
-
-	function toggleSidebar() {
-		showSidebar = !showSidebar;
-	}
+<script>
+	import App from '$lib/App.svelte';
 </script>
 
-<AppShell>
-	<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead"><h1>Baumstamm</h1></svelte:fragment>
-			<svelte:fragment slot="trail">
-				<button
-					type="button"
-					class="btn btn-icon"
-					class:variant-filled={showSidebar}
-					on:click={toggleSidebar}
-				>
-					{#if !showSidebar}
-						<Avatar initials="XY" />
-					{:else}
-						<i class="fa-solid fa-arrow-right" />
-					{/if}
-				</button>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-
-	<svelte:fragment slot="sidebarRight">
-		{#if showSidebar}
-			<div class="bg-surface-500/10 w-80 h-full">
-				<Sidebar />
-			</div>
-		{/if}
-	</svelte:fragment>
-	<TreeView />
-</AppShell>
+<App />
