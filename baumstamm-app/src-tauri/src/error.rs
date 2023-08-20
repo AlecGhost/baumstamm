1e479a5710de20fdc97be58629fdc04b5089cc9a
Alec Ghost <alexander.ghost@web.de>
2023-08-20 07:25:48 +0200

    fix: error serialization
    Errors should be serialized as strings.

--- baumstamm-app/src-tauri/src/error.rs
+++ baumstamm-app/src-tauri/src/error.rs
@@ -2,15 +2,17 @@ use serde::Serialize;
 use thiserror::Error;
 
 #[derive(Debug, Error, Serialize)]
+#[serde(untagged)]
 pub enum Error {
+    #[serde(serialize_with = "serialize_to_string")]
     #[error(transparent)]
     Baumstamm(#[from] baumstamm_lib::error::Error),
-    #[serde(serialize_with = "serialize_io_error")]
+    #[serde(serialize_with = "serialize_to_string")]
     #[error(transparent)]
     Io(#[from] std::io::Error),
 }
 
-fn serialize_io_error<S>(error: &std::io::Error, serializer: S) -> Result<S::Ok, S::Error>
+fn serialize_to_string<E: ToString, S>(error: &E, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
 {
