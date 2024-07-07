1d3965dae5dc0fecb1bd5f079b0dfa3d9c57ab97
Alec Ghost <alexander.ghost@web.de>
2024-07-07 10:53:03 +0200

    feat(elm): add profile pictures

--- baumstamm-app/src/Tree.elm
+++ baumstamm-app/src/Tree.elm
@@ -13,7 +13,7 @@ view { treeData, activePerson, onSelect } =
         viewItem item =
             el
                 [ width (px 200)
-                , height (px 100)
+                , height (px 300)
                 ]
                 (case item of
                     PersonItem pid ->
