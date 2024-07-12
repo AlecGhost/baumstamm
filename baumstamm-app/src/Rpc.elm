3bce0e78ce8df12fd8f6c889c3093e87b93b9d16
Alec Ghost <alexander.ghost@web.de>
2024-07-12 19:59:48 +0200

    feat(elm): more flags
    Include an initial state in the flags,
    containing settings and tree data.

--- baumstamm-app/src/Rpc.elm
+++ baumstamm-app/src/Rpc.elm
@@ -1,4 +1,13 @@
-port module Rpc exposing (Incoming(..), InsertInfoPayload, Outgoing(..), decodeIncoming, encodeOutgoing, receive, send)
+port module Rpc exposing
+    ( Incoming(..)
+    , InsertInfoPayload
+    , Outgoing(..)
+    , decodeIncoming
+    , decodeTreeData
+    , encodeOutgoing
+    , receive
+    , send
+    )
 
 import Data exposing (..)
 import Dict
@@ -83,119 +92,8 @@ decodeIncoming value =
     case proc of
         Ok "tree_data" ->
             let
-                decodePersons =
-                    Decode.list <|
-                        Decode.map2 Person
-                            (Decode.field "id" Decode.string)
-                            (Decode.field "info"
-                                (Decode.map (Maybe.withDefault Dict.empty)
-                                    (Decode.maybe (Decode.dict Decode.string))
-                                )
-                            )
-
-                decodeRelationships =
-                    Decode.list <|
-                        Decode.map3 Relationship
-                            (Decode.field "id" Decode.string)
-                            (Decode.field "parents"
-                                (Decode.map2 Tuple.pair
-                                    (Decode.index 0 (Decode.maybe Decode.string))
-                                    (Decode.index 1 (Decode.maybe Decode.string))
-                                )
-                            )
-                            (Decode.field "children" (Decode.list Decode.string))
-
-                decodeColor =
-                    Decode.map3 hsl
-                        (Decode.index 0 Decode.float)
-                        (Decode.index 1 Decode.float)
-                        (Decode.index 2 Decode.float)
-
-                decodeFraction =
-                    Decode.map2 Fraction
-                        (Decode.field "numerator" Decode.int)
-                        (Decode.field "denominator" Decode.int)
-
-                decodeOrientation =
-                    Decode.string
-                        |> Decode.andThen
-                            (\str ->
-                                case str of
-                                    "Up" ->
-                                        Decode.succeed Up
-
-                                    "Down" ->
-                                        Decode.succeed Down
-
-                                    _ ->
-                                        Decode.fail "Invalid orientation value"
-                            )
-
-                decodeOrigin =
-                    Decode.string
-                        |> Decode.andThen
-                            (\str ->
-                                case str of
-                                    "Left" ->
-                                        Decode.succeed Left
-
-                                    "Right" ->
-                                        Decode.succeed Right
-
-                                    "None" ->
-                                        Decode.succeed None
-
-                                    _ ->
-                                        Decode.fail "Invalid origin value"
-                            )
-
-                decodePassing =
-                    Decode.map3 Passing
-                        (Decode.field "rid" Decode.string)
-                        (Decode.field "color" decodeColor)
-                        (Decode.field "y_fraction" decodeFraction)
-
-                decodeEnding =
-                    Decode.map5 Ending
-                        (Decode.field "rid" Decode.string)
-                        (Decode.field "color" decodeColor)
-                        (Decode.field "origin" decodeOrigin)
-                        (Decode.field "x_fraction" decodeFraction)
-                        (Decode.field "y_fraction" decodeFraction)
-
-                decodeCrossing =
-                    Decode.map5 Crossing
-                        (Decode.field "rid" Decode.string)
-                        (Decode.field "color" decodeColor)
-                        (Decode.field "origin" decodeOrigin)
-                        (Decode.field "x_fraction" decodeFraction)
-                        (Decode.field "y_fraction" decodeFraction)
-
-                decodeGrid =
-                    Decode.list <|
-                        Decode.list <|
-                            Decode.oneOf
-                                [ Decode.map PersonItem (Decode.field "Person" Decode.string)
-                                , Decode.map ConnectionsItem
-                                    (Decode.field "Connections"
-                                        (Decode.map4
-                                            Connections
-                                            (Decode.field "orientation" decodeOrientation)
-                                            (Decode.field "passing" (Decode.list decodePassing))
-                                            (Decode.field "ending" (Decode.list decodeEnding))
-                                            (Decode.field "crossing" (Decode.list decodeCrossing))
-                                        )
-                                    )
-                                ]
-
-                treeDataPayload =
-                    Decode.map3 Data.TreeData
-                        (Decode.field "persons" decodePersons)
-                        (Decode.field "relationships" decodeRelationships)
-                        (Decode.field "grid" decodeGrid)
-
                 payload =
-                    Decode.decodeValue (Decode.field "payload" treeDataPayload) value
+                    Decode.decodeValue (Decode.field "payload" decodeTreeData) value
             in
             case payload of
                 Ok data ->
@@ -233,3 +131,120 @@ decodeIncoming value =
 
         Err _ ->
             NoProc
+
+
+decodeTreeData : Decode.Decoder TreeData
+decodeTreeData =
+    let
+        decodePersons =
+            Decode.list <|
+                Decode.map2 Person
+                    (Decode.field "id" Decode.string)
+                    (Decode.field "info"
+                        (Decode.map (Maybe.withDefault Dict.empty)
+                            (Decode.maybe (Decode.dict Decode.string))
+                        )
+                    )
+
+        decodeRelationships =
+            Decode.list <|
+                Decode.map3 Relationship
+                    (Decode.field "id" Decode.string)
+                    (Decode.field "parents"
+                        (Decode.map2 Tuple.pair
+                            (Decode.index 0 (Decode.maybe Decode.string))
+                            (Decode.index 1 (Decode.maybe Decode.string))
+                        )
+                    )
+                    (Decode.field "children" (Decode.list Decode.string))
+
+        decodeColor =
+            Decode.map3 hsl
+                (Decode.index 0 Decode.float)
+                (Decode.index 1 Decode.float)
+                (Decode.index 2 Decode.float)
+
+        decodeFraction =
+            Decode.map2 Fraction
+                (Decode.field "numerator" Decode.int)
+                (Decode.field "denominator" Decode.int)
+
+        decodeOrientation =
+            Decode.string
+                |> Decode.andThen
+                    (\str ->
+                        case str of
+                            "Up" ->
+                                Decode.succeed Up
+
+                            "Down" ->
+                                Decode.succeed Down
+
+                            _ ->
+                                Decode.fail "Invalid orientation value"
+                    )
+
+        decodeOrigin =
+            Decode.string
+                |> Decode.andThen
+                    (\str ->
+                        case str of
+                            "Left" ->
+                                Decode.succeed Left
+
+                            "Right" ->
+                                Decode.succeed Right
+
+                            "None" ->
+                                Decode.succeed None
+
+                            _ ->
+                                Decode.fail "Invalid origin value"
+                    )
+
+        decodePassing =
+            Decode.map3 Passing
+                (Decode.field "rid" Decode.string)
+                (Decode.field "color" decodeColor)
+                (Decode.field "y_fraction" decodeFraction)
+
+        decodeEnding =
+            Decode.map5 Ending
+                (Decode.field "rid" Decode.string)
+                (Decode.field "color" decodeColor)
+                (Decode.field "origin" decodeOrigin)
+                (Decode.field "x_fraction" decodeFraction)
+                (Decode.field "y_fraction" decodeFraction)
+
+        decodeCrossing =
+            Decode.map5 Crossing
+                (Decode.field "rid" Decode.string)
+                (Decode.field "color" decodeColor)
+                (Decode.field "origin" decodeOrigin)
+                (Decode.field "x_fraction" decodeFraction)
+                (Decode.field "y_fraction" decodeFraction)
+
+        decodeGrid =
+            Decode.list <|
+                Decode.list <|
+                    Decode.oneOf
+                        [ Decode.map PersonItem (Decode.field "Person" Decode.string)
+                        , Decode.map ConnectionsItem
+                            (Decode.field "Connections"
+                                (Decode.map4
+                                    Connections
+                                    (Decode.field "orientation" decodeOrientation)
+                                    (Decode.field "passing" (Decode.list decodePassing))
+                                    (Decode.field "ending" (Decode.list decodeEnding))
+                                    (Decode.field "crossing" (Decode.list decodeCrossing))
+                                )
+                            )
+                        ]
+
+        decode =
+            Decode.map3 Data.TreeData
+                (Decode.field "persons" decodePersons)
+                (Decode.field "relationships" decodeRelationships)
+                (Decode.field "grid" decodeGrid)
+    in
+    decode
